73c8ede6b91c410ac6a0c25f7b60fddc52d7e7e9
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-09 19:56:49 +0300

    Fix icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.polls.index') }}">
-    <a href="{{ route('back.polls.index') }}"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Опросы</span></a>
+    <a href="{{ route('back.polls.index') }}"><i class="fa fa-chart-bar"></i> <span class="nav-label">Опросы</span></a>
 </li>

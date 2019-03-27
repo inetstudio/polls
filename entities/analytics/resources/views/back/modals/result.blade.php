234851fd58e8d321d1b10798a256674cb61c4936
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-28 02:31:39 +0300

    Fixes

--- entities/analytics/resources/views/back/modals/result.blade.php
+++ entities/analytics/resources/views/back/modals/result.blade.php
@@ -1,12 +1,12 @@
 <div class="ibox-content">
-    <h2>{{ $item['question'] }}</h2>
+    <h2 class="m-b-lg">{{ $item['question'] }}</h2>
     <div>
         @foreach ($item['options'] as $option)
             <div>
                 <span>{{ $option['answer'] }}</span>
                 <small class="float-right">{{ $option['votes_percent'].'% ('.$option['votes'].')' }}</small>
             </div>
-            <div class="progress progress-small">
+            <div class="progress progress-small m-b-lg">
                 <div style="width: {{ $option['votes_percent'] }}%;" class="progress-bar"></div>
             </div>
         @endforeach
